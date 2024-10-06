8ce19599ce82f0955f1dce8961440f0c9344f466
UdaraWickramarathne <bimsaraudara25@gmail.com>
2024-10-07 03:15:45 +0530

    home screen create

---
+++ lib/components/title_text.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class TitleText extends StatelessWidget {
+  final String text;
+  const TitleText({super.key, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: const TextStyle(
+        color: Colors.white,
+        fontSize: 35,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,85 @@
+import 'package:financial_app/components/title_text.dart';
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Stack(
+        children: [
+          // Background Image
+          Container(
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/img1.jpg'),
+                fit: BoxFit.cover,
+                opacity: 0.7,
+              ),
+            ),
+          ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 50),
+                  const TitleText(text: 'The Smart'),
+                  const SizedBox(height: 10),
+                  const TitleText(text: 'App for'),
+                  const SizedBox(height: 10),
+                  const TitleText(text: 'Your Financial Life'),
+                  const SizedBox(height: 450),
+
+                  // Center-aligned Button
+                  Center(
+                    child: Column(
+                      children: [
+                        Container(
+                          width: 250,
+                          height: 55,
+                          decoration: BoxDecoration(
+                            gradient: const LinearGradient(
+                              colors: [
+                                Color.fromARGB(111, 255, 255, 255),
+                                Color.fromARGB(61, 255, 255, 255),
+                                Color.fromARGB(55, 255, 255, 255),
+                              ],
+                              begin: Alignment.centerLeft,
+                              end: Alignment.centerRight,
+                            ),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          child: const Center(
+                            child: Text(
+                              'Sign In',
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 20),
+                        const Text(
+                          'Create an Account',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }

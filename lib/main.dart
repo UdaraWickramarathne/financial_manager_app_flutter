1de79a08316c122e785bc9390cd7d3b64409503c
Udara Wickramarathne <bimsaraudara25@gmail.com>
2024-11-06 20:16:58 +0530

    reminder notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,22 @@
 import 'package:feedback/feedback.dart';
 import 'package:financial_app/adopt_a_wallet_app.dart';
 import 'package:financial_app/language/language_provider.dart';
+import 'package:financial_app/services/notification_service.dart';
 import 'package:financial_app/themes/themedata.dart';
 import 'package:financial_app/themes/themeprovider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:timezone/data/latest.dart' as tz;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  tz.initializeTimeZones();
+  await NotificationService.init();
   runApp(
     MultiProvider(
       providers: [
---
+++ lib/services/notification_service.dart
@@ -0,0 +1,107 @@
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/timezone.dart' as tz;
+
+class NotificationService {
+  static final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+
+  static Future<void> onDidReceiveNotification(
+      NotificationResponse notificationResponse) async {
+    // Handle different actions based on the actionId
+    if (notificationResponse.actionId == 'complete_action') {
+      // Logic for handling "Complete" action
+      print("Notification marked as complete.");
+      await flutterLocalNotificationsPlugin.cancel(notificationResponse.id!);
+    } else if (notificationResponse.actionId == 'snooze_action') {
+      // Logic for handling "Snooze" action
+      print("Notification snoozed.");
+      await snoozeNotification(notificationResponse.id!);
+    } else {
+      print("Notification received.");
+    }
+  }
+
+  static Future<void> init() async {
+    const AndroidInitializationSettings androidInitializationSettings =
+        AndroidInitializationSettings("@mipmap/ic_launcher");
+    const DarwinInitializationSettings iOSInitializationSettings =
+        DarwinInitializationSettings();
+
+    const InitializationSettings initializationSettings =
+        InitializationSettings(
+      android: androidInitializationSettings,
+      iOS: iOSInitializationSettings,
+    );
+
+    await flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+      onDidReceiveNotificationResponse: onDidReceiveNotification,
+      onDidReceiveBackgroundNotificationResponse: onDidReceiveNotification,
+    );
+  }
+
+  static Future<void> showInstantNotification(String title, String body) async {
+    const NotificationDetails platformChannelSpecifics = NotificationDetails(
+      android: AndroidNotificationDetails(
+        'instant_notification_channel_id',
+        'Instant Notifications',
+        importance: Importance.max,
+        priority: Priority.high,
+        playSound: true,
+        actions: [
+          AndroidNotificationAction('complete_action', 'Complete'),
+          AndroidNotificationAction('snooze_action', 'Snooze'),
+        ],
+      ),
+      iOS: DarwinNotificationDetails(
+        presentSound: true, // Play the default iOS notification sound
+      ),
+    );
+
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      title,
+      body,
+      platformChannelSpecifics,
+      payload: 'instant_notification',
+    );
+  }
+
+  static Future<void> scheduleNotification(
+      int id, String title, String body, DateTime scheduledTime) async {
+    await flutterLocalNotificationsPlugin.zonedSchedule(
+      id,
+      title,
+      body,
+      tz.TZDateTime.from(scheduledTime, tz.local),
+      const NotificationDetails(
+        android: AndroidNotificationDetails(
+          'reminder_channel',
+          'Reminder Channel',
+          importance: Importance.high,
+          priority: Priority.high,
+          playSound: true,
+          actions: [
+            AndroidNotificationAction('complete_action', 'Complete'),
+            AndroidNotificationAction('snooze_action', 'Snooze'),
+          ],
+        ),
+        iOS: DarwinNotificationDetails(
+          presentSound: true, // Play the default iOS notification sound
+        ),
+      ),
+      androidScheduleMode: AndroidScheduleMode.exactAllowWhileIdle,
+      uiLocalNotificationDateInterpretation:
+          UILocalNotificationDateInterpretation.absoluteTime,
+      matchDateTimeComponents: DateTimeComponents.dateAndTime,
+    );
+  }
+
+  static Future<void> snoozeNotification(int id) async {
+    // Snooze logic (e.g., 5 minutes from now)
+    final snoozeTime =
+        tz.TZDateTime.now(tz.local).add(const Duration(minutes: 1));
+    await scheduleNotification(
+        id, 'Reminder', 'Snoozed notification', snoozeTime);
+  }
+}
